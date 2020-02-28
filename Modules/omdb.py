5d240e0c8c8d5e3516083cd741cae78bed74fcee
Antonino Cangialosi <ninocangialosi@yahoo.it>
2020-02-28 23:47:57 +0200

    removed old code

---
+++ Modules/omdb.py
@@ -0,0 +1,150 @@
+from .requests_soup import *
+from .mysql_functions import *
+from bs4 import BeautifulSoup
+import time 
+from hashlib import sha1
+
+# defined variable for debugging purposes
+failed = False
+
+def omdb_search(title,year):
+    """ Search the Online Movie database from a title and year and return the content of the search if found, or boolean value False otherwise
+    """
+    #reads the key for the api and store it in the variable key
+    with open("api.key",'r') as f:
+        key = f.read().replace('\n','')
+    
+    #builds the url to be passed to the scraper
+    url = 'http://www.omdbapi.com/?apikey=' + key + '&t=' + title.replace(" ","+").replace("'",'+') +'&y='+ str(year) + '&r=xml'
+    request = simple_get(url) #sends a request to the url and returns a tuple (<format>,<raw_content>)
+    try:
+        content = BeautifulSoup(request[1],"lxml-"+request[0]) #parse the content
+
+        ################ move this here 
+        if content.root['response'] != 'False':
+            print("Found", title, "on OMDb!")
+            return content
+        else:
+            if len(shrink_title(title).split()) > 1:
+                omdb_search(shrink_title(title), year)
+            else:
+                print("NEED TO ADD ERROR TO THE TABLE for ", title, year)
+                # global failed
+                # failed = True
+                return False
+        ################ end of modification
+
+    except Exception as exc:
+        print(exc ,'occurred!')
+        time.sleep(5)
+        print('Retrying to get data from omdb')
+        omdb_search(title,year)
+        
+    # if content.root['response'] != 'False':
+    #     return content
+    # else:
+    #     if len(shrink_title(title).split()) > 1:
+    #         omdb_search(shrink_title(title), year)
+    #     else:
+    #         print("NEED TO ADD ERROR TO THE TABLE for ", title, year)
+    #         global failed
+    #         failed = True
+    #         return False
+
+
+def omdb_search_by_id(title,imdb_id):
+    """ Search the Online Movie database from a imbd ID  and return the content of the search if found, or boolean value False otherwise
+    """
+    #reads the key for the api and store it in the variable key
+    with open("api.key",'r') as f:
+        key = f.read().replace('\n','')
+    
+    #builds the url to be passed to the scraper
+    url = 'http://www.omdbapi.com/?apikey=' + key + '&i=' + imdb_id + '&r=xml'
+    request = simple_get(url) #sends a request to the url and returns a tuple (<format>,<raw_content>)
+    try:
+        content = BeautifulSoup(request[1],"lxml-"+request[0]) #parse the content
+
+        ################ move this here 
+        if content.root['response'] != 'False':
+            print("Found", title, "on OMDb!")
+            return content
+        else:
+            print("NEED TO ADD ERROR TO THE TABLE for ", title)
+            # nonlocal failed
+            # failed = True
+            return False
+        ################ end of modification
+
+    except Exception as exc:
+        print(exc ,'occurred!')
+        time.sleep(5)
+        print('Retrying to get data from omdb')
+        omdb_search_by_id(title,imdb_id)
+
+
+def shrink_title(title):
+    """ Shrink a string "title" by dropping the final word
+    """
+    with open("api.key",'r') as f:
+        key = f.read().replace('\n','')
+
+    last_word = title.split().pop()
+    new_title = title.rstrip(last_word).rstrip().rstrip("'")
+
+    return new_title
+
+
+def omdb_poster(movie):
+    """ Save the poster image to folder ./Posters
+    """
+    with open('api.key','r') as f:
+        key = f.read().replace('\n','')
+
+    url = 'http://img.omdbapi.com/?apikey=' + key + '&i=' + movie['imdb_id'] + '&h=300'
+    with get(url, stream= True) as resp:
+        if resp.status_code == 200:
+            name = movie['title'] + movie['year']
+            hasher = sha1(name.encode('utf-8'))
+
+            path_to_poster='./Posters/'+hasher.hexdigest()+'.jpg'
+
+            with open(path_to_poster,'wb') as f:
+                f.write(resp.content)
+  
+            return hasher.hexdigest()
+        else: 
+            return "nocover"
+
+def omdb_search_without_year(title):
+    """ Search the Online Movie database from just the title and return the content of the search if found, or boolean value False otherwise
+    """
+    #reads the key for the api and store it in the variable key
+    with open("api.key",'r') as f:
+        key = f.read().replace('\n','')
+    
+    #builds the url to be passed to the scraper
+    url = 'http://www.omdbapi.com/?apikey=' + key + '&t=' + title.replace(" ","+").replace("'",'+') + '&r=xml'
+    request = simple_get(url) #sends a request to the url and returns a tuple (<format>,<raw_content>)
+    try:
+        content = BeautifulSoup(request[1],"lxml-"+request[0]) #parse the content
+
+        ################ move this here 
+        if content.root['response'] != 'False':
+            print("Found", title, "on OMDb!")
+            return content
+        else:
+            if len(shrink_title(title).split()) > 1:
+                omdb_search_without_year(shrink_title(title))
+            else:
+                print("NEED TO ADD ERROR TO THE TABLE for ", title, year)
+                # nonlocal failed
+                # failed = True
+                return False
+        ################ end of modification
+
+    except Exception as exc:
+        print(exc ,'occurred!')
+        time.sleep(5)
+        print('Retrying to get data from omdb')
+        omdb_search(title,year)
---
+++ scrapp.py
@@ -0,0 +1,31 @@
+# loading package dependencies
+from Modules.hbo import hbo_scraper, hbo_series_scraper 
+from Modules.mysql_functions import connect_to_database, disconnect_database 
+# from Modules.requests_soup import *
+from Modules.viaplay import viaplay_scraper, viaplay_series_scraper 
+
+
+def main():
+    """ This is going to connect to mysql database and
+        scrape data from the various services
+    """
+
+    cnx, cursor = connect_to_database("database_credential.key")
+
+    print('Scraping data from the internet...')
+    # print("Scraping movies from HBO")
+    # hbo_scraper(cnx, cursor)
+    print("Scraping movies from viaplay")
+    viaplay_scraper(cnx, cursor)
+    # print("Scraping tv series from hbo")
+    # hbo_series_scraper(cnx, cursor)
+    # print("Scraping tv series from viaplay")
+    # viaplay_series_scraper(cnx, cursor)
+    print('Scraping complete!')
+
+    disconnect_database(cnx, cursor)
+    return
+
+
+if __name__ == "__main__":
+    main()
